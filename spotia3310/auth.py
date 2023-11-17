ba1f31772ad5c2c36138eaade3848fda8f5989f8
Kenan Henzelin <kenan.henzelin@proton.me>
2023-11-17 15:28:35 +0100

    Fixed .env bug

--- spotia3310/auth.py
+++ spotia3310/auth.py
@@ -2,14 +2,15 @@ import spotipy
 import os
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
-load_dotenv()
+dotenv_path = os.path.join(os.getcwd(), ".env")
+load_dotenv(dotenv_path)
 
 
 def user_library():
     sp = spotipy.Spotify(
         auth_manager=SpotifyOAuth(
             client_id=os.getenv("CLIENT_ID"),
-            client_secret=os.getenv(("CLIENT_SECRET")),
+            client_secret=os.getenv("CLIENT_SECRET"),
             redirect_uri="http://localhost:8888",
             scope="playlist-read-private",
         )
